1a41d29f9a26229ddef0e160d4a0948c67c1505f
Mihir Paghdal <mihirpaghdal29@gmail.com>
2022-06-14 12:02:00 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -140,3 +140,24 @@ class Dev(Configuration):
   # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
   DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+  }
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug("Got %d posts", len(posts))
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request,slug):
@@ -23,6 +27,7 @@ def post_detail(request,slug):
       comment_form = CommentForm()
   else:
     comment_form = None
+  logger.info("Created comment on Post %d for user %s", post.pk, request.user)
   return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
